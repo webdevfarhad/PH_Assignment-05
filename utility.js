fdf32b7bdbd906f1f6039f972a1ae07ecb47d9ee
webdevfarhad <muhammadhfarhad@gmail.com>
2024-02-20 22:29:58 +0600

    Interactive 'Buy button' added

---
+++ utility.js
@@ -0,0 +1,8 @@
+document.getElementById('buy-ticket-btn').addEventListener('click', function() {
+    document.getElementById('Departure-P1').scrollIntoView({ behavior: 'smooth' });
+});
+
+
+document.getElementById('A1').addEventListener('click', function(e){
+    console.log(e);
+})
