492cb42535378a3b8dd1f7e9d2c6bfeb57571cc1
webdevfarhad <muhammadhfarhad@gmail.com>
2024-02-21 19:49:25 +0600

    Check push

--- utility.js
+++ utility.js
@@ -4,5 +4,11 @@ document.getElementById('buy-ticket-btn').addEventListener('click', function() {
 
 
 document.getElementById('A1').addEventListener('click', function(e){
+    
+    document.getElementById('A1').style.backgroundColor= "#1DD100";
+    document.getElementById('A1').style.color= "white";
+    alert("Please consider my mistake Sir. i lost my job, in this race. i've family to feed so i dint have enough time to cover the dom module properly to do the technical part. but still i'm trying my best");
+    // const count;
     console.log(e);
+
 })
